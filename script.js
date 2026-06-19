14feccdfe4fd09bdfa379e492e7b2c26efdaa03a
lautarobilinski24-fix <lautarobilinski01@gmail.com>
2026-06-19 10:18:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,65 @@
 const canvas = document.getElementById("matrix");
 const ctx = canvas.getContext("2d");
 
-canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 const letters =
 "01ABCDEFGHIJKLMNOPQRSTUVWXYZ#$%&@";
+
 const fontSize = 14;
-const columns = canvas.width / fontSize;
+const columns = Math.floor(canvas.width / fontSize);
 
 const drops = [];
 
-for(let x = 0; x < columns; x++){
-drops[x] = 1;
+for(let i = 0; i < columns; i++){
+    drops[i] = 1;
 }
 
 function draw(){
 
-```
-ctx.fillStyle = "rgba(0,0,0,0.05)";
-ctx.fillRect(0,0,canvas.width,canvas.height);
-
-ctx.fillStyle = "#00ff88";
-ctx.font = fontSize + "px monospace";
-
-for(let i = 0; i < drops.length; i++){
-
-    const text =
-    letters.charAt(
-        Math.floor(Math.random() * letters.length)
+    ctx.fillStyle = "rgba(0,0,0,0.05)";
+    ctx.fillRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
     );
 
-    ctx.fillText(
-        text,
-        i * fontSize,
-        drops[i] * fontSize
-    );
+    ctx.fillStyle = "#00ff88";
+    ctx.font = fontSize + "px monospace";
 
-    if(
-        drops[i] * fontSize > canvas.height &&
-        Math.random() > 0.975
-    ){
-        drops[i] = 0;
+    for(let i = 0; i < drops.length; i++){
+
+        const text =
+        letters.charAt(
+            Math.floor(
+                Math.random() * letters.length
+            )
+        );
+
+        ctx.fillText(
+            text,
+            i * fontSize,
+            drops[i] * fontSize
+        );
+
+        if(
+            drops[i] * fontSize > canvas.height &&
+            Math.random() > 0.975
+        ){
+            drops[i] = 0;
+        }
+
+        drops[i]++;
     }
-
-    drops[i]++;
-}
-```
-
 }
 
-setInterval(draw,35);
+setInterval(draw, 35);
 
 window.addEventListener("resize", () => {
 
-```
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-```
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
 });
